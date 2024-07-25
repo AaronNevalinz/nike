80ce5466bae4affa153a63b97dc0c33ca2a4cade
AaronNevalinz <aaronug421@gmail.com>
2024-07-25 23:18:09 +0300

    nike website

---
+++ src/components/Nav.jsx
@@ -0,0 +1,53 @@
+
+import {headerLogo} from '../assets/images'
+import { hamburger } from '../assets/icons'
+import {navLinks} from '../constants'
+
+const Nav = () => {
+  return (
+    <header className='padding-x py-8 absolute z-10 w-full'>
+      <nav className='flex justify-between items-center max-container'>
+        <a href="/">
+          <img 
+            src={headerLogo} 
+            alt="logo" 
+            width={130}
+            height={29}
+            />
+        </a>
+
+        <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+          {
+            navLinks.map((item)=>(
+              <li key={item.label}>
+                <a 
+                  href={item.href} 
+                  className='font-montserrat leading-normal text-lg text-slate-gray'
+                >
+                  {item.label}
+                </a>
+              </li>
+            ))
+          }
+          
+        </ul>
+
+        <div className='font-bold font-montserrat hover:text-[#333] hidden lg:block'>
+            <a href="#">Sign in/Sign Up</a>
+          </div>
+
+        <div className='hidden max-lg:block'>
+          <img 
+            src={hamburger} 
+            alt="" 
+            width={25}
+            height={25}
+            className='cursor-pointer'
+            />
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Nav
